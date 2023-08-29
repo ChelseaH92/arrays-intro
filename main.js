dba4411a9afc3dd194782a2f588392e5f40d4251
Chelsea Hatton <chelseahatton1992@gmail.com>
2023-08-29 13:22:36 -0400

    arrays into completed

---
+++ main.js
@@ -0,0 +1,31 @@
+const yellowFruit = "Banana"
+const orangeFruit = "Orange"
+const redFruit = "Apple"
+const greenFruit = "Watermelon"
+const blueFruit = "Blueberry"
+
+const fruits = ['Banana', 'Orange', 'Apple', 'Watermelon', 'Blueberry']
+
+console.log(fruits)
+
+
+const dishes = [
+    "Dinner plate", "Water glass", "Salad bowl",
+    "Dinner plate", "Wine glass", "Spoon",
+    "Spoon", "Fork", "Salad bowl", "Whiskey glass",
+    "Fork", "Spoon", "Fork"
+]
+
+/*
+    Declare three variables to store the string value
+    of a glass in the array. Use the correct index to
+    get the right value.
+*/
+
+
+console.log("I am cleaning the following glasses:")
+
+// Put each variable in one of the parenthesis below
+console.log(dishes[1]);
+console.log(dishes[4]);
+console.log(dishes[9]);
